d23e607f9da4d1cb11104a32706b44f198ba21c5
Stefan Hagen <stefan@hagen.link>
2020-12-24 12:40:21 +0100

    Refactored further names and fixed some bugs - no tests still

--- src/entrypoint.py
+++ src/entrypoint.py
@@ -8,9 +8,9 @@ GITHUB_EVENT_NAME = os.environ["GITHUB_EVENT_NAME"]
 # Set repository
 CURRENT_REPOSITORY = os.environ["GITHUB_REPOSITORY"]
 # TODO: How about PRs from forks?
-INPUT_TARGET_REPOSITORY = os.environ["INPUT_TARGET_REPOSITORY"] or CURRENT_REPOSITORY
+INPUT_TARGET_REPOSITORY = os.getenv("INPUT_TARGET_REPOSITORY", CURRENT_REPOSITORY)
 INPUT_PULL_REQUEST_REPOSITORY = (
-        os.environ["INPUT_PULL_REQUEST_REPOSITORY"] or INPUT_TARGET_REPOSITORY
+        os.getenv("INPUT_PULL_REQUEST_REPOSITORY", INPUT_TARGET_REPOSITORY)
 )
 REPOSITORY = (
     INPUT_PULL_REQUEST_REPOSITORY
@@ -39,7 +39,7 @@ INPUT_GITHUB_TOKEN = os.environ["INPUT_GITHUB_TOKEN"]
 SCOPE_SEP = ","
 KNOWN_SCOPES = (
     "all",
-    "informaton",
+    "information",
     "missingInclude",
     "performance",
     "portability",
@@ -48,32 +48,32 @@ KNOWN_SCOPES = (
     "warning",
 )
 # domain specific language between environment and cppcheck parameters:
-INPUT_CHECK_LIBRARY = "INPUT_CHECK_LIBRARY"
-INPUT_SKIP_PREPROCESSOR = "INPUT_SKIP_PREPROCESSOR"
-INPUT_ENABLE = "INPUT_ENABLE"
-INPUT_EXCLUDE_CHECK = "INPUT_EXCLUDE_CHECK"
-INPUT_INCONCLUSIVE = "INPUT_INCONCLUSIVE"
-INPUT_INLINE_SUPPRESSION = "INPUT_INLINE_SUPPRESSION"
-INPUT_FORCE_LANGUAGE = "INPUT_FORCE_LANGUAGE"
-INPUT_MAX_CTU_DEPTH = "INPUT_MAX_CTU_DEPTH"
-INPUT_OUTPUT_FILE = "INPUT_OUTPUT_FILE"
-INPUT_PLATFORM = "INPUT_PLATFORM"
+CHECK_LIBRARY = "INPUT_CHECK_LIBRARY"
+SKIP_PREPROCESSOR = "INPUT_SKIP_PREPROCESSOR"
+ENABLE_CHECKS = "INPUT_ENABLE"
+EXCLUDE_CHECK = "INPUT_EXCLUDE_CHECK"
+ENABLE_INCONCLUSIVE = "INPUT_INCONCLUSIVE"
+INLINE_SUPPRESSION = "INPUT_INLINE_SUPPRESSION"
+ENFORCE_LANGUAGE = "INPUT_FORCE_LANGUAGE"
+MAX_CTU_DEPTH = "INPUT_MAX_CTU_DEPTH"
+OUTPUT_FILE = "INPUT_OUTPUT_FILE"
+PLATFORM_TYPE = "INPUT_PLATFORM"
 
 # domain specific vocabulary for switches:
-DISABLED = 'disabled'
-ENABLED = 'enabled'
+DISABLED = 'disable'
+ENABLED = 'enable'
 
 DSL = {
-    INPUT_CHECK_LIBRARY: os.getenv(INPUT_CHECK_LIBRARY, "disable"),
-    INPUT_SKIP_PREPROCESSOR: os.getenv(INPUT_SKIP_PREPROCESSOR, "disable"),
-    INPUT_ENABLE: os.getenv(INPUT_ENABLE, "all"),
-    INPUT_EXCLUDE_CHECK: os.getenv(INPUT_EXCLUDE_CHECK, "disable"),
-    INPUT_INCONCLUSIVE: os.getenv(INPUT_INCONCLUSIVE, "enable"),
-    INPUT_INLINE_SUPPRESSION: os.getenv(INPUT_INLINE_SUPPRESSION, "disable"),
-    INPUT_FORCE_LANGUAGE: os.getenv(INPUT_FORCE_LANGUAGE, "disable"),
-    INPUT_MAX_CTU_DEPTH: os.getenv(INPUT_MAX_CTU_DEPTH, "disable"),
-    INPUT_OUTPUT_FILE: os.getenv(INPUT_OUTPUT_FILE, "cppcheck_report.txt"),
-    INPUT_PLATFORM: os.getenv(INPUT_PLATFORM, "disable"),
+    CHECK_LIBRARY: os.getenv(CHECK_LIBRARY, "disable"),
+    SKIP_PREPROCESSOR: os.getenv(SKIP_PREPROCESSOR, "disable"),
+    ENABLE_CHECKS: os.getenv(ENABLE_CHECKS, "all"),
+    EXCLUDE_CHECK: os.getenv(EXCLUDE_CHECK, "disable"),
+    ENABLE_INCONCLUSIVE: os.getenv(ENABLE_INCONCLUSIVE, "enable"),
+    INLINE_SUPPRESSION: os.getenv(INLINE_SUPPRESSION, "disable"),
+    ENFORCE_LANGUAGE: os.getenv(ENFORCE_LANGUAGE, "disable"),
+    MAX_CTU_DEPTH: os.getenv(MAX_CTU_DEPTH, "disable"),
+    OUTPUT_FILE: os.getenv(OUTPUT_FILE, "cppcheck_report.txt"),
+    PLATFORM_TYPE: os.getenv(PLATFORM_TYPE, "disable"),
 }
 
 
@@ -83,7 +83,7 @@ def split_csv(text):
         for scope in text:
             yield scope.strip().lower()
     else:
-        yield scope.strip().lower()
+        yield text.strip().lower()
 
 
 def is_valid(scope):
@@ -93,7 +93,7 @@ def is_valid(scope):
 
 def parse_scopes():
     """Return the parsed scopes."""
-    scopes = set(t for t in split_csv(DSL[INPUT_ENABLE]) if is_valid(t))
+    scopes = set(t for t in split_csv(DSL[ENABLE_CHECKS]) if is_valid(t))
     if "all" in scopes:
         scopes = ["all"]
     else:
@@ -108,40 +108,26 @@ def command():
         f"--enable={SCOPE_SEP.join(parse_scopes())}",
     ]
 
-    actions = {
-        INPUT_CHECK_LIBRARY: (operator.eq, ENABLED, "--check-library"),
-        INPUT_SKIP_PREPROCESSOR: (operator.eq, ENABLED, "-E"),
-        INPUT_INLINE_SUPPRESSION: (operator.eq, ENABLED, "--inline-suppr"),
-        INPUT_INCONCLUSIVE: (operator.eq, DISABLED, "--inconclusive"),
-
-        INPUT_EXCLUDE_CHECK: (operator.eq, DISABLED, "-i {{}}"),
-        INPUT_FORCE_LANGUAGE: (operator.eq, DISABLED, "--language={{}}}"),
-        INPUT_MAX_CTU_DEPTH: (operator.eq, DISABLED, "--max-ctu-depth={{}}}"),
-        INPUT_PLATFORM: (operator.ne, DISABLED, "--platform={{}}"),
+    constant_actions = 4
+    actions = {  # group by arity of actions to simplify processing below
+        # constant actions:
+        CHECK_LIBRARY: (operator.eq, ENABLED, "--check-library"),
+        SKIP_PREPROCESSOR: (operator.eq, ENABLED, "-E"),
+        INLINE_SUPPRESSION: (operator.eq, ENABLED, "--inline-suppr"),
+        ENABLE_INCONCLUSIVE: (operator.eq, DISABLED, "--inconclusive"),
+        # unary actions:
+        EXCLUDE_CHECK: (operator.eq, DISABLED, "-i {{}}"),
+        ENFORCE_LANGUAGE: (operator.eq, DISABLED, "--language={{}}}"),
+        MAX_CTU_DEPTH: (operator.eq, DISABLED, "--max-ctu-depth={{}}}"),
+        PLATFORM_TYPE: (operator.ne, DISABLED, "--platform={{}}"),
     }
+    constant_dimensions = tuple(actions.keys())[:constant_actions]
 
-    constant_dimensions = (
-        INPUT_CHECK_LIBRARY,
-        INPUT_SKIP_PREPROCESSOR,
-        INPUT_INLINE_SUPPRESSION,
-        INPUT_INCONCLUSIVE)
-
-    unary_dimensions = (
-        INPUT_EXCLUDE_CHECK,
-        INPUT_FORCE_LANGUAGE,
-        INPUT_MAX_CTU_DEPTH,
-        INPUT_PLATFORM)
-
-    for dim in constant_dimensions:
-        predicate, ref, contribution = actions[dim]
-        if predicate(DSL[dim], ref):
-            vector.append(contribution)
-
-    for dim in unary_dimensions:
-        predicate, ref, contribution = actions[dim]
+    for dim in actions:
+        predicate, ref, template = actions[dim]
         payload = DSL[dim]
         if predicate(payload, ref):
-            vector.append(contribution.format(payload))
+            vector.append(template if dim in constant_dimensions else template.format(payload))
 
     return vector
 
@@ -150,7 +136,7 @@ def run(vector, where=".", show_version=None, show_help=None):
     """Execute the command in a sub process."""
     show_version = show_version if show_version is None else True
     show_help = show_help if show_help is None else True
-    vector.append(f"--output-file={DSL[INPUT_OUTPUT_FILE]}")
+    vector.append(f"--output-file={DSL[OUTPUT_FILE]}")
     vector.append(f"{where}")
     print(f"given command {' '.join(vector)}")
 
