699099f5508feb60a76ff6ca3f2ebc2c3481459d
Dipankar Pal <dipankarpal5050@gmail.com>
2020-12-25 19:57:23 +0530

    Update entrypoint.py

--- src/entrypoint.py
+++ src/entrypoint.py
@@ -1,154 +1,170 @@
+"""Drive the parameter collection and execution for cppcheck."""
+import operator
 import os
-import subprocess as sp
+import subprocess
 
+# The following environment reads will fail execution if variables not set:
 GITHUB_EVENT_NAME = os.environ["GITHUB_EVENT_NAME"]
-
 # Set repository
 CURRENT_REPOSITORY = os.environ["GITHUB_REPOSITORY"]
-# TODO: How about PRs from forks?
-TARGET_REPOSITORY = os.environ["INPUT_TARGET_REPOSITORY"] or CURRENT_REPOSITORY
-PULL_REQUEST_REPOSITORY = (os.environ["INPUT_PULL_REQUEST_REPOSITORY"]
-                           or TARGET_REPOSITORY)
-REPOSITORY = (PULL_REQUEST_REPOSITORY
-              if GITHUB_EVENT_NAME == "pull_request" else TARGET_REPOSITORY)
-
 # Set branches
 GITHUB_REF = os.environ["GITHUB_REF"]
 GITHUB_HEAD_REF = os.environ["GITHUB_HEAD_REF"]
 GITHUB_BASE_REF = os.environ["GITHUB_BASE_REF"]
-CURRENT_BRANCH = GITHUB_HEAD_REF or GITHUB_REF.rsplit("/", 1)[-1]
-TARGET_BRANCH = os.environ["INPUT_TARGET_BRANCH"] or CURRENT_BRANCH
-PULL_REQUEST_BRANCH = os.environ["INPUT_PULL_REQUEST_BRANCH"] or GITHUB_BASE_REF
-BRANCH = PULL_REQUEST_BRANCH if GITHUB_EVENT_NAME == "pull_request" else TARGET_BRANCH
-
+# Owners and tokens
 GITHUB_ACTOR = os.environ["GITHUB_ACTOR"]
 GITHUB_REPOSITORY_OWNER = os.environ["GITHUB_REPOSITORY_OWNER"]
-GITHUB_TOKEN = os.environ["INPUT_GITHUB_TOKEN"]
+INPUT_GITHUB_TOKEN = os.environ["INPUT_GITHUB_TOKEN"]
 
-# command related inputs
+# Derive from environment with defaults:
+# TODO: How about PRs from forks?
+INPUT_TARGET_REPOSITORY = os.getenv("INPUT_TARGET_REPOSITORY", CURRENT_REPOSITORY)
+INPUT_PULL_REQUEST_REPOSITORY = (
+        os.getenv("INPUT_PULL_REQUEST_REPOSITORY", INPUT_TARGET_REPOSITORY)
+)
+REPOSITORY = (
+    INPUT_PULL_REQUEST_REPOSITORY
+    if GITHUB_EVENT_NAME == "pull_request"
+    else INPUT_TARGET_REPOSITORY
+)
 
-CHECK_LIBRARY = os.environ["INPUT_CHECK_LIBRARY"] or "disable"
-SKIP_PREPROCESSOR = os.environ["INPUT_SKIP_PREPROCESSOR"] or "disable"
-ENABLE = os.environ["INPUT_ENABLE"] or "all"
-EXCLUDE_CHECK = os.environ["INPUT_EXCLUDE_CHECK"] or "disable"
-INCONCLUSIVE = os.environ["INPUT_INCONCLUSIVE"] or "enable"
-INLINE_SUPPRESSION = os.environ["INPUT_INLINE_SUPPRESSION"] or "disable"
-FORCE_LANGUAGE = os.environ["INPUT_FORCE_LANGUAGE"] or "disable"
-MAX_CTU_DEPTH = os.environ["INPUT_MAX_CTU_DEPTH"] or "disable"
-OUTPUT_FILE = os.environ["INPUT_OUTPUT_FILE"] or "cppcheck_report.txt"
-PLATFORM = os.environ["INPUT_PLATFORM"] or "disable"
-
-# GITHUB_USER = os.environ["INPUT_GITHUB_USERNAME"] or "cppcheck-action"
-# GITHUB_EMAIL = os.environ["INPUT_GITHUB_EMAIL"] or "cppcheck-action@master"
-# COMMIT_MSG = os.environ[
-#     "INPUT_COMMIT_MSG"] or "cppcheck report added or updated"
-
-command = ""
-
-
-def prepare_command():
-    global command
-    global out_file
-    command = command + "cppcheck "
-    # check every flags
-
-    if CHECK_LIBRARY == "enable":
-        command = command + " --check-library"
-
-    if SKIP_PREPROCESSOR == "enable":
-        command = command + " -E"
-
-    enable_val = "all"  # default fallback value
-
-    if ENABLE == "warning":
-        enable_val = "warning"
-    elif ENABLE == "style":
-        enable_val = "style"
-    elif ENABLE == "performance":
-        enable_val = "performance"
-    elif ENABLE == "portability":
-        enable_val = "portability"
-    elif ENABLE == "information":
-        enable_val = "information"
-    elif ENABLE == "unusedFunction":
-        enable_val = "unusedFunction"
-    elif ENABLE == "missingInclude":
-        enable_val = "missingInclude"
-
-    # multiple checks ; comma separated , skipping additional error checking
-    if "," in ENABLE:
-        enable_val = ENABLE
-
-    command = command + f" --enable={enable_val}"
-
-    if EXCLUDE_CHECK != "disable":
-        command = command + f" -i {EXCLUDE_CHECK}"
-        # assuming user passes a valid path
-    if INCONCLUSIVE != "disable":
-        command = command + " --inconclusive"
-
-    if INLINE_SUPPRESSION == "enable":
-        command = command + " --inline-suppr"
-
-    if FORCE_LANGUAGE != "disable":
-        command = command + f" --language={FORCE_LANGUAGE}"
-    if MAX_CTU_DEPTH != "disable":
-        command = command + f" --max-ctu-depth={MAX_CTU_DEPTH}"
-
-    if PLATFORM != "disable":
-        command = command + f" --platform={PLATFORM}"
-
-    out_file = OUTPUT_FILE
+CURRENT_BRANCH = GITHUB_HEAD_REF or GITHUB_REF.rsplit("/", 1)[-1]
+INPUT_TARGET_BRANCH = os.getenv("INPUT_TARGET_BRANCH", CURRENT_BRANCH)
+INPUT_PULL_REQUEST_BRANCH = os.getenv("INPUT_PULL_REQUEST_BRANCH", GITHUB_BASE_REF)
+BRANCH = (
+    INPUT_PULL_REQUEST_BRANCH
+    if GITHUB_EVENT_NAME == "pull_request"
+    else INPUT_TARGET_BRANCH
+)
 
 
-def run_cppcheck():
-    global command
-    command = command + f" --output-file={out_file} ."
-    print("given command " + command)
-    print("checking version")
-    sp.call("cppcheck --version", shell=True)
-    sp.call("cppcheck --help",shell=True)
-    sp.call(command, shell=True)
+# Define cppcheck specific vocabulary for switches:
+DISABLED = 'disable'
+ENABLED = 'enable'
+CHECK_EVERYTHING = 'all'
+
+CHECKS_SEP = ","
+KNOWN_CHECKS = (
+    CHECK_EVERYTHING,
+    "information",
+    "missingInclude",
+    "performance",
+    "portability",
+    "style",
+    "unusedFunction",
+    "warning",
+)
+# Domain specific mapping between environment and cppcheck parameters:
+CHECK_LIBRARY = "INPUT_CHECK_LIBRARY"
+SKIP_PREPROCESSOR = "INPUT_SKIP_PREPROCESSOR"
+ENABLE_CHECKS = "INPUT_ENABLE"
+EXCLUDE_CHECK = "INPUT_EXCLUDE_CHECK"
+ENABLE_INCONCLUSIVE = "INPUT_INCONCLUSIVE"
+INLINE_SUPPRESSION = "INPUT_INLINE_SUPPRESSION"
+ENFORCE_LANGUAGE = "INPUT_FORCE_LANGUAGE"
+MAX_CTU_DEPTH = "INPUT_MAX_CTU_DEPTH"
+OUTPUT_FILE = "INPUT_OUTPUT_FILE"
+PLATFORM_TYPE = "INPUT_PLATFORM"
+
+# Main interface map for cppcheck instrumentation and outputs:
+DSL = {
+    CHECK_LIBRARY: os.getenv(CHECK_LIBRARY, DISABLED),
+    SKIP_PREPROCESSOR: os.getenv(SKIP_PREPROCESSOR, DISABLED),
+    ENABLE_CHECKS: os.getenv(ENABLE_CHECKS, CHECK_EVERYTHING),
+    EXCLUDE_CHECK: os.getenv(EXCLUDE_CHECK, DISABLED),
+    ENABLE_INCONCLUSIVE: os.getenv(ENABLE_INCONCLUSIVE, ENABLED),
+    INLINE_SUPPRESSION: os.getenv(INLINE_SUPPRESSION, DISABLED),
+    ENFORCE_LANGUAGE: os.getenv(ENFORCE_LANGUAGE, DISABLED),
+    MAX_CTU_DEPTH: os.getenv(MAX_CTU_DEPTH, DISABLED),
+    OUTPUT_FILE: os.getenv(OUTPUT_FILE, "cppcheck_report.txt"),
+    PLATFORM_TYPE: os.getenv(PLATFORM_TYPE, DISABLED),
+}
+
+# Prepare actions to be taken using the above environment interface map:
+CONSTANT_ACTIONS = 4
+ACTIONS = {  # group by arity of actions to simplify processing below
+    # constant actions:
+    CHECK_LIBRARY: (operator.eq, ENABLED, "--check-library"),
+    SKIP_PREPROCESSOR: (operator.eq, ENABLED, "-E"),
+    INLINE_SUPPRESSION: (operator.eq, ENABLED, "--inline-suppr"),
+    ENABLE_INCONCLUSIVE: (operator.ne, DISABLED, "--inconclusive"),
+    # unary actions:
+    EXCLUDE_CHECK: (operator.ne, DISABLED, "-i {{}}"),
+    ENFORCE_LANGUAGE: (operator.ne, DISABLED, "--language={}"),
+    MAX_CTU_DEPTH: (operator.ne, DISABLED, "--max-ctu-depth={}"),
+    PLATFORM_TYPE: (operator.ne, DISABLED, "--platform={}"),
+}
+CONSTANT_DIMENSIONS = tuple(ACTIONS.keys())[:CONSTANT_ACTIONS]
 
 
-# def commit_changes():
-#     """Commits changes."""
-#     set_email = f"git config --local  user.email {GITHUB_EMAIL}"
-#     set_user = f"git config --local  user.name {GITHUB_USER}"
-
-#     sp.call(set_email, shell=True)
-#     sp.call(set_user, shell=True)
-
-#     git_checkout = f"git checkout {TARGET_BRANCH}"
-#     git_add = f"git add {out_file}"
-#     git_commit = f'git commit -m  "{COMMIT_MSG}"'
-
-#     print("Committing reports.......")
-
-#     sp.call(git_checkout, shell=True)
-#     sp.call(git_add, shell=True)
-#     sp.call(git_commit, shell=True)
+def split_csv(text):
+    """Naive split of text as comma separated check aspects yielding as-input case strings."""
+    if CHECKS_SEP in text:
+        for check in text.split(CHECKS_SEP):
+            yield check.strip()
+    else:
+        yield text.strip()
 
 
-# def push_changes():
-#     """Pushes commit."""
-#     set_url = f"git remote set-url origin https://x-access-token:{GITHUB_TOKEN}@github.com/{TARGET_REPOSITORY}"
-#     git_push = f"git push origin {TARGET_BRANCH}"
-#     sp.call(set_url, shell=True)
-#     sp.call(git_push, shell=True)
+def is_valid(check):
+    """Return scope if valid else empty string."""
+    return check if check in KNOWN_CHECKS else ""
+
+
+def parse_checks(dsl):
+    """Return the parsed checks."""
+    checks = set(t for t in split_csv(dsl[ENABLE_CHECKS]) if is_valid(t))
+    if CHECK_EVERYTHING in checks:
+        checks = [CHECK_EVERYTHING]
+    else:
+        checks = sorted(checks)
+    return checks
+
+
+def command(dsl=None, actions=None, checks_sep=CHECKS_SEP, constant_dimensions=CONSTANT_DIMENSIONS):
+    """Prepare the command vector and set the path to the report file"""
+    dsl = DSL if dsl is None else dsl
+    actions = ACTIONS if actions is None else actions
+
+    vector = [
+        "cppcheck",
+        f"--enable={checks_sep.join(parse_checks(dsl))}",
+    ]
+
+    for dim in actions:
+        predicate, ref, template = actions[dim]
+        payload = dsl[dim]
+        if predicate(payload, ref):
+            vector.append(template if dim in constant_dimensions else template.format(payload))
+
+    return vector
+
+
+def run(vector, where=".", show_version=None, show_help=None):
+    """Execute the command in a sub process."""
+    show_version = show_version is None
+    show_help = show_help is None
+    vector.append(f"--output-file={DSL[OUTPUT_FILE]}")
+    vector.append(f"{where}")
+    print(f"given command {' '.join(vector)}")
+
+    if show_version:
+        print("checking version")
+        subprocess.call("cppcheck --version", shell=True)
+
+    if show_help:
+        subprocess.call("cppcheck --help", shell=True)
+
+    subprocess.call(vector, shell=True)
 
 
 def main():
-
-    if (GITHUB_EVENT_NAME == "pull_request") and (GITHUB_ACTOR !=
-                                                  GITHUB_REPOSITORY_OWNER):
+    """Drive the parameter extraction and execution of cppcheck."""
+    if all((GITHUB_EVENT_NAME == "pull_request", GITHUB_ACTOR != GITHUB_REPOSITORY_OWNER)):
         return
 
-    prepare_command()
-    run_cppcheck()
-    # commit_changes()
-    # push_changes()
+    run(command())
 
 
 if __name__ == "__main__":
-    main()
+    main()  # pragma: no cover
